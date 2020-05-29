ad92fbd3019bebed8bfaef2308bf865d586a4cbe
Diego Gaston Arias Galeano <diegood@gmail.com>
2020-05-29 19:25:34 -0300

    jugando con el Sw

--- sw.js
+++ sw.js
@@ -8,10 +8,47 @@ self.addEventListener('activate', e => {
 })
 
 self.addEventListener('fetch', e => {
-    console.log( 'fetch: ')
-    console.log( e.request)
+    console.log('sw fetch')
+    // let url = e.request.url
+    // let respuesta
+
+    // if(url.includes('estilos.css')){
+    //     console.log('estilo')
+    //     // capturo el archivo de estilos.css  y mando otro en su lugar
+    //     // respuesta = new Response(
+    //     //                 `body{background-color: red;}`,
+    //     //                 { headers: { 'content-type' : 'text/css' } }
+    //     //             )
+    //     respuesta = fetch(url)
+
+    // }
+    // else if(url.includes('material.light_blue-deep_purple')){
+    //     respuesta = fetch('https://code.getmdl.io/1.3.0/material.indigo-pink.min.css')
+    // } 
+    // else if(url.includes('super.jpg')){
+    //     respuesta = fetch('images/coto.jpg')
+    // } 
+    // else{
+    //     respuesta = fetch(url) 
+    // }
+
+    // e.respondWith(respuesta)
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 self.addEventListener('push', e => {
     console.log('mandaron una push', e)
