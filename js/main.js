4f4e8c6f641dbffa17ee593ad6a6886d7f140d8e
Diego Gaston Arias Galeano <diegood@gmail.com>
2020-05-29 20:08:06 -0300

    agrego jquery

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ console.log('inicia la app')
 
 let dialog = document.querySelector('dialog');
 let crearLista = true
-let ul
 let listaProductos = [
     { nombre: 'Pan', cantidad: 2, precio: 12.34 },
     { nombre: 'Carne', cantidad: 3, precio: 54.13 },
@@ -15,35 +14,35 @@ function borrarProducto(index) {
     renderLista()
 }
 function cambiarCantidad(index, e) {
-    let cantidad = Number(e.value)
+    let cantidad = Number(e.val())
     listaProductos[index].cantidad = cantidad
     renderLista()
 }
 
 function cambiarPrecio(index, e) {
-    let precio = Number(e.value)
+    let precio = Number(e.val())
     listaProductos[index].precio = precio
     renderLista()
 }
 
 
 function configurarlistners() {
-    document.getElementById('btn-entrada-producto').addEventListener('click', (e) => {
+    $('#btn-entrada-producto').click(() => {
         // comprabar que no sea null el input y armar bien el objeto
-        let nombreProducto= document.getElementById('ingreso-producto').value
+        let nombreProducto= $('#ingreso-producto').val()
         if(!nombreProducto){
             dialog.showModal()
         }else{
             listaProductos.push({ nombre: nombreProducto, cantidad: 1, precio:0 })
-            document.getElementById('ingreso-producto').value = null
+            $('#ingreso-producto').val(null)
             renderLista()
         }
     })
-    document.getElementById('btn-borrar-lista').addEventListener('click', (e) => {
+    $('#btn-borrar-lista').click(() => {
         listaProductos = []
         renderLista()
     })
-    dialog.querySelector('.close').addEventListener('click', function () {
+    dialog.querySelector('.close').click( function () {
         dialog.close();
     });
 }
@@ -58,10 +57,9 @@ function renderLista() {
             let source = xhr.response
             const template = Handlebars.compile(source)
             let data = {"listaProductos" : listaProductos}
-            let lista = document.getElementById('lista')
-            lista.innerHTML = template(data)
+            $('#lista').html(template(data))
         
-            let ul = document.getElementById('contenedor-lista')
+            let ul = $('#contenedor-lista')
             componentHandler.upgradeElements(ul);
         }
     })
@@ -82,6 +80,10 @@ function registrarServiceWorker(){
     }
 }
 
-renderLista()
-configurarlistners()
-registrarServiceWorker()
+function start(){
+    renderLista()
+    configurarlistners()
+    registrarServiceWorker()
+}
+
+$(document).ready(start)
