26d234edb2bbf7d4af665687fbc7529fd7d4674d
Diego Gaston Arias Galeano <diegood@gmail.com>
2020-05-29 21:04:53 -0300

    traigo productos de mockapi

--- js/main.js
+++ js/main.js
@@ -3,11 +3,30 @@ console.log('inicia la app')
 let dialog = document.querySelector('dialog');
 let crearLista = true
 let listaProductos = [
-    { nombre: 'Pan', cantidad: 2, precio: 12.34 },
-    { nombre: 'Carne', cantidad: 3, precio: 54.13 },
-    { nombre: 'Leche', cantidad: 4, precio: 22.32 },
-    { nombre: 'Fideos', cantidad: 4, precio: 78.34 }
+    // { nombre: 'Pan', cantidad: 2, precio: 12.34 },
+    // { nombre: 'Carne', cantidad: 3, precio: 54.13 },
+    // { nombre: 'Leche', cantidad: 4, precio: 22.32 },
+    // { nombre: 'Fideos', cantidad: 4, precio: 78.34 }
 ]
+
+function getURL(){
+    return 'https://5ed19a894e6d7200163a0a2b.mockapi.io/lista/'
+}
+
+function getProdWeb(callBack){
+    let url = getURL()+'?'+Date.now()
+    $.ajax({url: url, method: 'get' })
+    .then(callBack)
+    .catch(e => {
+        console.log(e)
+        callBack(listaProductos)
+    })
+}
+
+
+
+
+
 function borrarProducto(index) {
     console.log(listaProductos);
     listaProductos.splice(index, 1);
@@ -50,44 +69,20 @@ function configurarlistners() {
 
 function renderLista() {
 
-    $.get('plantatilla-lista.hbs', source =>{
+    $.ajax({url: 'plantatilla-lista.hbs', method : 'get'})
+    .then(source =>{
         const template = Handlebars.compile(source)
-        let data = {"listaProductos" : listaProductos}
-        $('#lista').html(template(data))
-    
-        let ul = $('#contenedor-lista')
-        componentHandler.upgradeElements(ul);
-    })
 
-
-    // fetch('plantatilla-lista.hbs')
-    //     .then(res => res.text())
-    //     .then( source => {
-    //             console.log(source)
-    //             const template = Handlebars.compile(source)
-    //             let data = {"listaProductos" : listaProductos}
-    //             $('#lista').html(template(data))
-            
-    //             let ul = $('#contenedor-lista')
-    //             componentHandler.upgradeElements(ul);
-    //         }
-    //     )
-
-
-    // let xhr = new XMLHttpRequest
-    // xhr.open('get', 'plantatilla-lista.hbs')
-    // xhr.send()
-    // xhr.addEventListener('load', () =>{
-    //     if(xhr.status == 200){
-    //         let source = xhr.response
-    //         const template = Handlebars.compile(source)
-    //         let data = {"listaProductos" : listaProductos}
-    //         $('#lista').html(template(data))
+        getProdWeb(productos =>{
+            listaProductos = productos
+            let data = {"listaProductos" : listaProductos}
+            $('#lista').html(template(data))
         
-    //         let ul = $('#contenedor-lista')
-    //         componentHandler.upgradeElements(ul);
-    //     }
-    // })
+            let ul = $('#contenedor-lista')
+            componentHandler.upgradeElements(ul);
+        })
+
+    })
 
 }
 
